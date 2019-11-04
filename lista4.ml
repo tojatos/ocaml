816914d6afb0772b2d0afdc192430a0f3c7ff024
tojatos <tojatos@gmail.com>
2019-11-04 12:57:42 +0100

    lista4 zad5 done

--- lista4.ml
+++ lista4.ml
@@ -51,3 +51,26 @@ let tt = Node(1,
                    )
               );;
 print_list_int (breadthBT tt);;
+
+print_endline "## Zad 5";;
+
+let depthSearch (Graph graph) startNode =
+  let rec search visited = function
+    | [] -> []
+    | h::t -> if List.mem h visited then search visited t
+    else h::search (h::visited) (graph h @ t)
+  in search [] [startNode]
+;;
+
+
+let g = Graph
+(function
+  0 -> [3]
+    | 1 -> [0;2;4]
+    | 2 -> [1]
+    | 3 -> []
+    | 4 -> [0;2]
+    | n -> failwith ("Graph g: node "^string_of_int n^" doesn't exist")
+);;
+
+print_list_int (depthSearch g 4);;
