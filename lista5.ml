158e391e001d50f11b372ba31ee8cda42be9bcf0
tojatos <tojatos@gmail.com>
2019-11-17 18:50:52 +0100

    lista5 done

--- lista5.ml
+++ lista5.ml
@@ -54,8 +54,37 @@ print_endline "## Zad 2";;
 let lfib =
   let rec lfibIter a b =
     LCons(a, lazy(lfibIter b (a+b)))
-  in
-  lfibIter 0 1
+  in lfibIter 0 1
 ;;
 
 print_list_int (ltake (11, lfib));;
+
+print_endline "## Zad 3";;
+
+let rec (@$) ll1 ll2 =
+  match ll1 with
+    | LNil -> ll2
+    | LCons(x, lazy xf) -> LCons(x, lazy (xf @$ ll2))
+;;
+
+
+type 'a lBT = LEmpty | LNode of 'a * (unit -> 'a lBT) * (unit -> 'a lBT);;
+
+let lBreadth bt =
+  let rec iter = function
+    | [] -> LNil
+    | LEmpty::tl -> iter tl
+    | LNode(v, bt1, bt2)::tl -> LCons(v, lazy(iter (tl @ [bt1(); bt2()])))
+  in iter [bt]
+;;
+
+let rec lTree n = LNode(n, (fun () -> lTree (2*n)), fun () -> lTree (2*n+1));;
+
+let e = (fun () -> LEmpty);;
+let tree = LNode(1, (fun () -> LNode(4, e, e)), fun () -> LNode(7, e, e));;
+
+print_list_int (ltake (21, lBreadth tree));;
+print_list_int (ltake (71, lBreadth (lTree 0)));;
+print_list_int (ltake (21, lBreadth (lTree 1)));;
+print_list_int (ltake (21, lBreadth (lTree 2)));;
+print_list_int (ltake (21, lBreadth (lTree 3)));;
